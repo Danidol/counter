0ec2418f1f31597bb17859fb51da905017e83625
Daniel <danicek76@yahoo.com>
2021-02-26 16:01:37 +0100

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      EyeBlinked: 0,
+    }
+  }
+
+  clickHandler = () => {
+    this.setState({ EyeBlinked: this.state.EyeBlinked + 1 })
+  }
+
+  clickHandler01 = () => {
+    if (this.state.EyeBlinked === 0) {
+      this.setState({ EyeBlinked: 0 })
+    } else {
+      this.setState({ EyeBlinked: this.state.EyeBlinked - 1 })
+    }
+  }
+
+  clickHandler02 = () => {
+    this.setState({ EyeBlinked: 0 })
+  }
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1>{`Today you blinked: ${this.state.EyeBlinked} times`}</h1>
+
+          <div className="btns">
+            <button onClick={this.clickHandler}>Click +</button>
+            <button onClick={this.clickHandler01}>Click -</button>
+            <button onClick={this.clickHandler02}>Reset</button>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
